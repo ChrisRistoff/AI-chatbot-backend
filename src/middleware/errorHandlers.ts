f78a68a24b68523c5472e430838fb848d0a7ab54
ChrisRistoff <doanusho@gmail.com>
2025-03-22 14:19:32 +0000

    errorHandling
    add basic error handling

---
+++ src/middleware/errorHandlers.ts
@@ -0,0 +1,48 @@
+import { NextFunction, Request, Response } from "express";
+
+export function sqlErrors(err: any, _: Request, res: Response, next: NextFunction): void {
+    switch(err.code) {
+        case("22P02"):
+            res.status(400).send({ msg: "Invalid input" });
+            break;
+        case("23503"):
+            res.status(400).send({ msg: "Bad request" });
+            break;
+        case("23505"):
+            res.status(409).send({ msg: "Already exists" });
+            break;
+        case "23502":
+            res.status(400).send({ msg: "Not null violation" });
+            break;
+        case "42703":
+            res.status(400).send({ msg: "Undefined column" });
+            break;
+        case "42P01":
+            res.status(400).send({ msg: "Undefined table" });
+            break;
+        case "22001":
+            res.status(400).send({ msg: "String data right truncation" });
+            break;
+        case "22003":
+            res.status(400).send({ msg: "Numeric value out of range" });
+            break;
+        case "40001":
+            res.status(409).send({ msg: "Serialization failure (possible deadlock)" });
+            break;
+        default:
+            next(err);
+    }
+};
+
+export function customErrors(err: any, _: Request, res: Response, next: NextFunction): void {
+    if (err.errCode) {
+        res.status(err.errCode).send({ msg: err.errMsg });
+    } else {
+        next(err);
+    }
+};
+
+export function serverError(err: any, _: Request, res: Response, __: NextFunction): void {
+    console.log(err);
+    res.status(500).send({ msg: "Internal server error" });
+};
